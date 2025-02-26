0efb8343f1df9bc675eea073c6a7a3629f26d188
romain noyer <romain.noyer@gmail.com>
2025-02-26 18:46:47 +0100

    add chart lib

---
+++ src/components/Barchart.jsx
@@ -0,0 +1,13 @@
+import "./Barchart.scss";
+import { BarChart } from "recharts";
+
+function Barchart() {
+  return (
+    <>
+      <p>bar chart</p>
+      <BarChart />
+    </>
+  );
+}
+
+export default Barchart;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,39 @@
 import "./Dashboard.scss";
 import Hero from './Hero'
 import Card from "./Card";
+import Barchart from "./Barchart";
 import fire from "../assets/icons/fire.svg";
 import chicken from "../assets/icons/chicken.svg";
 import apple from "../assets/icons/apple.svg";
 import burger from "../assets/icons/burger.svg";
+import { useState, useEffect } from "react";
 
-function Dashboard( firstName, todayScore, calorieCount, proteinCount, CarboCount, lipidCount ) {
-  calorieCount = proteinCount = CarboCount = lipidCount = 1930;
+function Dashboard() {
+
+  // const [calorieCount, setFirstName] = useState()
+  // const [proteinCount, setProteinCount] = useState()
+  // const [carboCount, setCarboCount] = useState()
+  // const [lipidCount, setLipidCount] = useState()
+  
+  // useEffect(() => {
+  //   const response
+  // })
+
+  const calorieCount = 1930;
+  const proteinCount = 1930;
+  const carboCount = 1930;
+  const lipidCount = 1930;
+
+
 
   return (
     <div className="dash">
       <div className="dash--hero">
         <Hero />
       </div>
-      <div className="dash--bar-chart">bar chart</div>
+      <div className="dash--bar-chart">
+        <Barchart />
+      </div>
       <div className="dash--line-chart">line chart</div>
       <div className="dash--radar-chart">radar chart</div>
       <div className="dash--radial-chart">radial chart</div>
@@ -34,7 +53,7 @@ function Dashboard( firstName, todayScore, calorieCount, proteinCount, CarboCoun
           bgColor="#4AB8FF"
         />
         <Card
-          value={CarboCount}
+          value={carboCount}
           unit="g"
           category="Glucides"
           iconPath={apple}
